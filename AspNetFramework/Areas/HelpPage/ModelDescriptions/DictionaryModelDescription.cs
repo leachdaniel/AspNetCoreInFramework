16243f57374b7d3d49487b08eb0710c15c0a1872
Daniel Leach <theleachster@hotmail.com>
2018-01-14 08:57:47 -0600

    clean up uneeded projects

--- AspNetFramework/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AspNetFramework.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- AspNetFramework/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AspNetFramework.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
